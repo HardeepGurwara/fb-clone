a04269d3c19a6471832a4a28950d0c60d146cfe3
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-10-13 03:08:14 +0100

    cleaned up the app, made the header component and styled it

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBc8YAJD2ymj0Pn4sKuq6FB0toA91zbXvk",
+    authDomain: "fb-clone-32be5.firebaseapp.com",
+    databaseURL: "https://fb-clone-32be5.firebaseio.com",
+    projectId: "fb-clone-32be5",
+    storageBucket: "fb-clone-32be5.appspot.com",
+    messagingSenderId: "896905591520",
+    appId: "1:896905591520:web:c22b470c331cc93200ae85",
+    measurementId: "G-P57C89ZSGP"
+  };
