4f1d8ac6c1148ee90a7eb02076568c5ec8600841
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2020-10-15 20:45:16 +0100

    made the story reel component and styled it

---
+++ src/Feed.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Feed.css";
+import StoryReel from "./StoryReel";
+function Feed() {
+  return (
+    <div className="feed">
+      {/*StoryReel  */}
+      <StoryReel />
+      {/* MessageSender */}
+    </div>
+  );
+}
+
+export default Feed;
---
+++ src/StoryReel.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Story from "./Story";
+import "./StoryReel.css";
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <Story
+        profileSrc="https://scontent.fewr1-6.fna.fbcdn.net/v/t31.0-8/12525616_992187174160176_8578738980750367535_o.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=WI2YNhjmLEYAX9uIFco&_nc_ht=scontent.fewr1-6.fna&oh=62888d9c2eb65797a242a693391d8620&oe=5F934DBE"
+        title="Victor Atencio"
+        image="https://scontent.fewr1-6.fna.fbcdn.net/v/t1.0-9/61224504_2267413489970865_7043756450023211008_n.jpg?_nc_cat=101&_nc_sid=8bfeb9&_nc_ohc=vSScxa8tNGEAX8J8cR-&_nc_ht=scontent.fewr1-6.fna&oh=ea0a3b8cc563eb7d5d8c45cba63a66aa&oe=5F95503A"
+      />
+      <Story
+        profileSrc="https://scontent.fewr1-6.fna.fbcdn.net/v/t1.0-9/119182517_2433455810292734_6471837175182699486_o.jpg?_nc_cat=108&_nc_sid=09cbfe&_nc_ohc=qwxNVgC1nJYAX_LRs0v&_nc_ht=scontent.fewr1-6.fna&oh=9931f91c2e232d8b4b7de4fd5fe1911f&oe=5F95330F"
+        title="Armando Atencio"
+        image="https://scontent.fewr1-6.fna.fbcdn.net/v/t1.0-9/120132648_2443636502607998_7089118356660786653_o.jpg?_nc_cat=100&_nc_sid=8bfeb9&_nc_ohc=U6see8MB22AAX82chad&_nc_ht=scontent.fewr1-6.fna&oh=4ff19c2546f78ab4a8041f7234ee3b72&oe=5F967B5E"
+      />
+      <Story
+        profileSrc="https://scontent.fewr1-5.fna.fbcdn.net/v/t1.0-9/69030897_10103359689911542_2823371445801844736_n.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_ohc=VSW2Et_M74oAX8pAinP&_nc_ht=scontent.fewr1-5.fna&oh=b9789076e993e7caa19c62af73c50156&oe=5F95189D"
+        title="Brendalisse Paris"
+        image="https://scontent.fewr1-6.fna.fbcdn.net/v/t1.0-9/119063448_4327811143956563_2513238669093786141_o.jpg?_nc_cat=102&_nc_sid=a26aad&_nc_ohc=eZ50QrqEXX8AX85tcW4&_nc_ht=scontent.fewr1-6.fna&oh=ca73b63f6f3c213702f68ddba9350ba1&oe=5F95BEFB"
+      />
+      <Story
+        profileSrc="https://scontent.fewr1-6.fna.fbcdn.net/v/t1.0-9/68741005_10219332979412824_3679415473650270208_o.jpg?_nc_cat=102&_nc_sid=09cbfe&_nc_ohc=J30E31J2-zsAX-BRAGj&_nc_ht=scontent.fewr1-6.fna&oh=52a145d1d636d419b6e0bb17f5c5de26&oe=5F96001B"
+        title="Alejandro Atencio"
+        image="https://scontent.fewr1-6.fna.fbcdn.net/v/t1.0-9/67932738_10219300147672051_1758127317676195840_n.jpg?_nc_cat=108&_nc_sid=8bfeb9&_nc_ohc=IDRd5f5-dksAX8_2l_B&_nc_ht=scontent.fewr1-6.fna&oh=fc4e4125c7702d5cd086ec58e19afb59&oe=5F9316EA"
+      />
+      <Story
+        profileSrc="https://scontent.fewr1-5.fna.fbcdn.net/v/t1.0-9/109358699_10157118291072175_3875336969848906087_o.jpg?_nc_cat=111&_nc_sid=174925&_nc_ohc=Vvk_cW4vSvQAX_qHhAT&_nc_ht=scontent.fewr1-5.fna&oh=57040a49651908b29852e74dbe80885c&oe=5F933E14"
+        title="Henry Velis"
+        image="https://scontent.fewr1-5.fna.fbcdn.net/v/t1.0-9/116794120_10157157541117175_7335248224371763019_o.jpg?_nc_cat=104&_nc_sid=e3f864&_nc_ohc=WAFHFnYk17cAX_lg68-&_nc_ht=scontent.fewr1-5.fna&oh=0d2d42675f9b173a42cbd8c2231937b3&oe=5F95B188"
+      />
+    </div>
+  );
+}
+
+export default StoryReel;
